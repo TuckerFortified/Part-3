e3aedc06ddc83c2db027e20f10c4f1698b9df78e
fortietu <fortietu@OA-S141-08>
2024-03-13 16:00:48 -0400

    Made it so text at the top of the screen displays the players avatar

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,10 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterControl : MonoBehaviour
 {
+
+    public TextMeshProUGUI Text;
+    public static CharacterControl Instance;
+
+    private void Start()
+    {
+        Instance = this; 
+    }
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -14,6 +24,7 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        CharacterControl.Instance.Text.text = villager.GetType().ToString();
     }
     
 }
