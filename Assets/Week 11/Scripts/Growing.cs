ae282cfceec8afb0c83012e686bd93c3ffd7e13e
fortietu <fortietu@OA-S141-08>
2024-03-27 14:05:32 -0400

    Made the shape grow functions a coroutine.

---
+++ Assets/Week 11/Scripts/Growing.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Growing : MonoBehaviour
+{
+    public GameObject square;
+    public GameObject triangle;
+    public GameObject circle;
+    public TextMeshProUGUI squareTMP;
+    public TextMeshProUGUI triangleTMP;
+    public TextMeshProUGUI circleTMP;
+    public TextMeshProUGUI crTMP;
+    public int running;
+    Coroutine coroutine;
+
+    void Start()
+    {
+        StartCoroutine(Square());
+        StartCoroutine(Triangle());
+        StartCoroutine(Circle());
+    }
+
+    void Update()
+    {
+        crTMP.text = "Coroutines: " + running.ToString();
+    }
+
+    IEnumerator Square()
+    {
+        running += 1;
+        float size = 0;
+        while (size < 5)
+        {
+            size += Time.deltaTime;
+            Vector3 scale = new Vector3(size, size, size);
+            square.transform.localScale = scale;
+            squareTMP.text = "Square: " + scale;
+            yield return null;
+        }
+        running -= 1;
+    }
+    IEnumerator Triangle()
+    {
+        running += 1;
+        float size = 0;
+        while (size < 5)
+        {
+            size += Time.deltaTime;
+            Vector3 scale = new Vector3(size, size, size);
+            triangle.transform.localScale = scale;
+            triangleTMP.text = "Triangle: " + scale;
+            yield return null;
+        }
+        running -= 1;
+    }
+    IEnumerator Circle()
+    {
+        running += 1;
+
+        float size = 0;
+        while (size < 5)
+        {
+            size += Time.deltaTime;
+            Vector3 scale = new Vector3(size, size, size);
+            circle.transform.localScale = scale;
+            circleTMP.text = "Cirlce: " + scale;
+            yield return null;
+        }
+        while (size > 0)
+        {
+            size -= Time.deltaTime;
+            Vector3 scale = new Vector3(size, size, size);
+            circle.transform.localScale = scale;
+            circleTMP.text = "Cirlce: " + scale;
+            yield return null;  
+        }
+        running -= 1;
+    }
+}
