ca27f0cf5da92fba6e5b3abd36c45ca9d37fc4bb
Tucker Fortier <tucker.fortier@gmail.com>
2024-03-28 20:43:54 -0400

    Made it so the button changes the position of the platform.

--- Assets/Assignment/Scripts/PlatformButton.cs
+++ Assets/Assignment/Scripts/PlatformButton.cs
@@ -5,31 +5,38 @@ using UnityEngine;
 public class PlatformButton : MonoBehaviour
 {
     public GameObject Platform;
+    public GameObject Position1;
+    public GameObject Position2;
     public int PlatformPosition = 1;
     public bool TouchingButton = false;
     Rigidbody2D rb;
-    // Start is called before the first frame update
+  
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
+
+        //If the player is touching the switch and pressing the E key, switch platform position.
         if (RedSquare.Switch == true && TouchingButton == true)
         {
             if (PlatformPosition == 1)
             {
+                Platform.transform.position = Position2.transform.position;
                 PlatformPosition = 2;
             }
             else if (PlatformPosition == 2) 
             {
+                Platform.transform.position = Position1.transform.position;
                 PlatformPosition = 1;
             }
         }
     }
 
+    //This code checks if the player is touching the button.
     private void OnTriggerEnter2D(Collider2D collision)
     {
         TouchingButton = true;
